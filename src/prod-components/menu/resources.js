90f911d7da96675cff445234cbae4141344a4c4c
Ben Gican II <b3nhur77@gmail.com>
2025-07-22 00:29:40 +0800

    remove pricing and quantity limit

--- src/prod-components/menu/resources.js
+++ src/prod-components/menu/resources.js
@@ -1,53 +1,43 @@
-import * as yup from 'yup';
+import * as yup from "yup";
 
 export const MENU_SCHEMA = yup.object().shape({
-  itemName: yup.string().required('Item name is required'),
+  itemName: yup.string().required("Item name is required"),
 });
 
 export const MENU_INPUT = [
-  { type: 'text', name: 'itemName', label: 'Item name' },
-  { type: 'textarea', name: 'description', label: 'Description' },
-  { type: 'image', name: 'image', label: 'Image' },
+  { type: "text", name: "itemName", label: "Item name" },
+  { type: "textarea", name: "description", label: "Description" },
+  { type: "image", name: "image", label: "Image" },
 ];
 
 export const VARIATION_SCHEMA = yup.object().shape({
   size: yup.string(),
-  qty: yup
-    .number()
-    .typeError('Amount must be a number')
-    .required('Please provide an item price.')
-    .min(0, 'At least one is required')
-    .max(5000, 'that is impossible!!!'),
-  price: yup
-    .number()
-    .typeError('Amount must be a number')
-    .required('Please provide an item price.')
-    .min(0, 'Too little')
-    .max(5000, 'Very costly!'),
+  qty: yup.number().typeError("Amount must be a number").required("Please provide an item price.").min(0, "At least one is required"),
+  price: yup.number().typeError("Amount must be a number").required("Please provide an item price.").min(0, "Price can not be zero."),
 });
 
 export const VARIATION_INPUT = [
-  { type: 'text', name: 'size', label: 'Size' },
-  { type: 'number', name: 'qty', label: 'Quantity' },
-  { type: 'number', name: 'price', label: 'Price' },
+  { type: "text", name: "size", label: "Size" },
+  { type: "number", name: "qty", label: "Quantity" },
+  { type: "number", name: "price", label: "Price" },
 ];
 
 export const VARIATION_HEADERS = [
-  { display: 'Size', name: 'size' },
-  { display: 'Qty', name: 'qty' },
-  { display: 'Price', name: 'price' },
+  { display: "Size", name: "size" },
+  { display: "Qty", name: "qty" },
+  { display: "Price", name: "price" },
 ];
 
 export const CUSTOMER_HEADERS = [
-  { display: 'Name', name: 'name' },
-  { display: 'Address', name: 'address' },
-  { display: 'Block', name: 'block' },
-  { display: 'Lot', name: 'lot' },
+  { display: "Name", name: "name" },
+  { display: "Address", name: "address" },
+  { display: "Block", name: "block" },
+  { display: "Lot", name: "lot" },
 ];
 
-export const MENU_HEADER = [{ display: 'Item name', name: 'itemName' }];
-export const DEFAULT_MENU_ITEM = { itemName: '', description: '', imageUrl: '' };
-export const DEFAULT_VARIATION_DATA = { size: 'small', qty: 1, price: 100 };
+export const MENU_HEADER = [{ display: "Item name", name: "itemName" }];
+export const DEFAULT_MENU_ITEM = { itemName: "", description: "", imageUrl: "" };
+export const DEFAULT_VARIATION_DATA = { size: "small", qty: 1, price: 100 };
 
 export const sortData = (menu) => {
   let sortedData = menu.sort((a, b) => {
